a64011fe22b3cb001f6e7ec02385ef254116b625
Ryan Johnson <jryaa7@gmail.com>
2024-10-02 16:14:58 -0600

    updates for missing tags

--- input-create.sh
+++ input-create.sh
@@ -43,73 +43,73 @@ fi
 
 # CAM
 echo "CAM"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/components/cam/cime_config/config_component.xml --comp CAM --htmlfile $OUTROOT/cam_input.html --compversion 6.0
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/components/cam/cime_config/config_component.xml --comp CAM --htmlfile $OUTROOT/cam_input.html --compversion 6.0
 
 # DATM
 echo "DATM"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/cime/src/components/data_comps/datm/cime_config/config_component.xml --comp DATM --htmlfile $OUTROOT/datm_input.html --compversion 2.1
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/cime/src/components/data_comps/datm/cime_config/config_component.xml --comp DATM --htmlfile $OUTROOT/datm_input.html --compversion 2.1
 
 # CLM
 echo "CLM5"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/components/clm/cime_config/config_component.xml --comp CLM --htmlfile $OUTROOT/clm5_0_input.html --comptag release-clm5.0.25
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/components/clm/cime_config/config_component.xml --comp CLM --htmlfile $OUTROOT/clm5_0_input.html --comptag release-clm5.0.25
 
 # DLND
 echo "DLND"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/cime/src/components/data_comps/dlnd/cime_config/config_component.xml --comp DLND --htmlfile $OUTROOT/dlnd_input.html --compversion 2.0
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/cime/src/components/data_comps/dlnd/cime_config/config_component.xml --comp DLND --htmlfile $OUTROOT/dlnd_input.html --compversion 2.0
 
 # MOSART
 echo "MOSART"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/components/mosart/cime_config/config_component.xml --comp MOSART --htmlfile $OUTROOT/mosart_input.html
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/components/mosart/cime_config/config_component.xml --comp MOSART --htmlfile $OUTROOT/mosart_input.html
 
 # RTM
 echo "RTM"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/components/rtm/cime_config/config_component.xml --comp RTM --htmlfile $OUTROOT/rtm_input.html
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/components/rtm/cime_config/config_component.xml --comp RTM --htmlfile $OUTROOT/rtm_input.html
 
 # DROF
 echo "DROF"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/cime/src/components/data_comps/drof/cime_config/config_component.xml --comp DROF --htmlfile $OUTROOT/drof_input.html --compversion 2.0
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/cime/src/components/data_comps/drof/cime_config/config_component.xml --comp DROF --htmlfile $OUTROOT/drof_input.html --compversion 2.0
 
 # POP2
 # pop namelist required *a lot* of manual edits to get it parse through CIME tools and namelist_defaults.xml is kinda hopeless
 echo "POP2"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/components/pop/cime_config/config_component.xml --comp POP2 --htmlfile $OUTROOT/pop2_input.html
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/components/pop/cime_config/config_component.xml --comp POP2 --htmlfile $OUTROOT/pop2_input.html
 
 # DOCN 
 # required a tweek to the "QOBS" string in DOCN_MODE desc because the quotes were invalid ascii characters
 echo "DOCN"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/cime/src/components/data_comps/docn/cime_config/config_component.xml --comp DOCN --htmlfile $OUTROOT/docn_input.html --compversion 2.0
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/cime/src/components/data_comps/docn/cime_config/config_component.xml --comp DOCN --htmlfile $OUTROOT/docn_input.html --compversion 2.0
 
 # CICE
 echo "CICE"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/components/cice/cime_config/config_component.xml --comp CICE --htmlfile $OUTROOT/cice_input.html --compversion 5.0
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/components/cice/cime_config/config_component.xml --comp CICE --htmlfile $OUTROOT/cice_input.html --compversion 5.0
 
 # DICE
 echo "DICE"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/cime/src/components/data_comps/dice/cime_config/config_component.xml --comp DICE --htmlfile $OUTROOT/dice_input.html --compversion 2.0
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/cime/src/components/data_comps/dice/cime_config/config_component.xml --comp DICE --htmlfile $OUTROOT/dice_input.html --compversion 2.0
 
 # WW3
 echo "WW3"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/components/ww3/cime_config/config_component.xml --comp WW3 --htmlfile $OUTROOT/ww3_input.html
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/components/ww3/cime_config/config_component.xml --comp WW3 --htmlfile $OUTROOT/ww3_input.html
 
 # DWAV
 echo "DWAV"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/cime/src/components/data_comps/dwav/cime_config/config_component.xml --comp DWAV --htmlfile $OUTROOT/dwav_input.html --compversion 2.0
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/cime/src/components/data_comps/dwav/cime_config/config_component.xml --comp DWAV --htmlfile $OUTROOT/dwav_input.html --compversion 2.0
 
 # CISM
 echo "CISM"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/components/cism/cime_config/config_component.xml --comp CISM --htmlfile $OUTROOT/cism_input.html
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/components/cism/cime_config/config_component.xml --comp CISM --htmlfile $OUTROOT/cism_input.html
 
 # Driver
 echo "DRIVER"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/cime/src/drivers/mct/cime_config/config_component.xml --comp Driver --htmlfile $OUTROOT/drv_input.html
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/cime/src/drivers/mct/cime_config/config_component.xml --comp Driver --htmlfile $OUTROOT/drv_input.html
 
 # Driver input cesm
 echo "DRIVER CESM"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/cime/src/drivers/mct/cime_config/config_component_cesm.xml --comp Driver --htmlfile $OUTROOT/drv_input_cesm.html
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/cime/src/drivers/mct/cime_config/config_component_cesm.xml --comp Driver --htmlfile $OUTROOT/drv_input_cesm.html
 
 # DESP
 echo "DESP"
-python input-html.py --cesmmodel 2.1.5 --inputfile $CESMROOT/cime/src/components/data_comps/desp/cime_config/config_component.xml --comp DESP --htmlfile $OUTROOT/desp_input.html --compversion 2.0
+python input-html.py --cesmmodel 2.1.4 --inputfile $CESMROOT/cime/src/components/data_comps/desp/cime_config/config_component.xml --comp DESP --htmlfile $OUTROOT/desp_input.html --compversion 2.0
 
 
 
--- namelist-create.sh
+++ namelist-create.sh
@@ -43,87 +43,87 @@ fi
 
 # CAM
 echo "CAM"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/components/cam/bld/namelist_files/namelist_definition.xml --comp CAM --htmlfile cam_nml.html --compversion 6.0
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/components/cam/bld/namelist_files/namelist_definition.xml --comp CAM --htmlfile cam_nml.html --compversion 6.0
 
 # DATM
 echo "DATM"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/cime/src/components/data_comps/datm/cime_config/namelist_definition_datm.xml --comp DATM --htmlfile datm_nml.html --compversion 2.0
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/cime/src/components/data_comps/datm/cime_config/namelist_definition_datm.xml --comp DATM --htmlfile datm_nml.html --compversion 2.0
 
 # CLM5
 # also desc elements, need to be pre formatted in order to wrap correctly in the data table
 echo "CLM5"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/components/clm/bld/namelist_files/namelist_definition_clm4_5.xml --comp CLM --htmlfile clm5_0_nml.html
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/components/clm/bld/namelist_files/namelist_definition_clm4_5.xml --comp CLM --htmlfile clm5_0_nml.html
 # CLM4
 #echo "CLM4"
-#python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/components/clm/bld/namelist_files/namelist_definition_clm4_0.xml --comp CLM --htmlfile clm4_0_nml.html
+#python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/components/clm/bld/namelist_files/namelist_definition_clm4_0.xml --comp CLM --htmlfile clm4_0_nml.html
 # CLM4.5
 # Error: unable to parse file namelist_definition_clm4_5.xml
 #echo "CLM4.5"
-#python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/components/clm/bld/namelist_files/namelist_definition_clm4_5.xml --comp CLM --htmlfile clm4_5_nml.html
+#python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/components/clm/bld/namelist_files/namelist_definition_clm4_5.xml --comp CLM --htmlfile clm4_5_nml.html
 
 # DLND
 echo "DLND"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/cime/src/components/data_comps/dlnd/cime_config/namelist_definition_dlnd.xml --comp DLND --htmlfile dlnd_nml.html --compversion 2.0
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/cime/src/components/data_comps/dlnd/cime_config/namelist_definition_dlnd.xml --comp DLND --htmlfile dlnd_nml.html --compversion 2.0
 
 # MOSART
 echo "MOSART"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/components/mosart/cime_config/namelist_definition_mosart.xml --comp MOSART --htmlfile mosart_nml.html
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/components/mosart/cime_config/namelist_definition_mosart.xml --comp MOSART --htmlfile mosart_nml.html
 
 # RTM
 echo "RTM"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/components/rtm/cime_config/namelist_definition_rtm.xml --comp RTM --htmlfile rtm_nml.html
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/components/rtm/cime_config/namelist_definition_rtm.xml --comp RTM --htmlfile rtm_nml.html
 
 # DROF
 echo "DROF"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/cime/src/components/data_comps/drof/cime_config/namelist_definition_drof.xml --comp DROF --htmlfile drof_nml.html --compversion 2.0
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/cime/src/components/data_comps/drof/cime_config/namelist_definition_drof.xml --comp DROF --htmlfile drof_nml.html --compversion 2.0
 
 # POP2
 echo "POP"
 # pop namelist required *a lot* of manual edits to get it to parse through the CIME tools and namelist_defaults.xml is kinda hopeless
-# python namelist-html.py --cesmmodel 2.1.5 --nmlfile ./pop_namelist_files/namelist_definition_pop.xml --comp POP2 --htmlfile pop2_nml.html --comptag pop2_cesm2_1_rel_n06
+# python namelist-html.py --cesmmodel 2.1.4 --nmlfile ./pop_namelist_files/namelist_definition_pop.xml --comp POP2 --htmlfile pop2_nml.html --comptag pop2_cesm2_1_rel_n06
 # TODO FIND POP2 XML FILES
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/components/pop/bld/namelist_files/namelist_definition_pop.xml --comp POP --htmlfile pop2_nml.html --compversion 2.0
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/components/pop/bld/namelist_files/namelist_definition_pop.xml --comp POP --htmlfile pop2_nml.html --compversion 2.0
 
 # MARBL
 # need to run the $MARBLROOT/MARBL_tools/yaml_to_json.py script first
 echo "MARBL"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/components/pop/externals/MARBL/defaults/json/settings_latest.json --comp MARBL --htmlfile marbl_nml.html --marbl-json
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/components/pop/externals/MARBL/defaults/json/settings_latest.json --comp MARBL --htmlfile marbl_nml.html --marbl-json
 
 # DOCN
 echo "DOCN"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/cime/src/components/data_comps/docn/cime_config/namelist_definition_docn.xml --comp DOCN --htmlfile docn_nml.html --compversion 2.0
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/cime/src/components/data_comps/docn/cime_config/namelist_definition_docn.xml --comp DOCN --htmlfile docn_nml.html --compversion 2.0
 
 # CICE
 echo "CICE"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/components/cice/cime_config/namelist_definition_cice.xml --comp CICE --htmlfile cice_nml.html  --compversion 5.0
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/components/cice/cime_config/namelist_definition_cice.xml --comp CICE --htmlfile cice_nml.html  --compversion 5.0
 
 # DICE
 echo "DICE"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/cime/src/components/data_comps/dice/cime_config/namelist_definition_dice.xml --comp DICE --htmlfile dice_nml.html --compversion 2.0
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/cime/src/components/data_comps/dice/cime_config/namelist_definition_dice.xml --comp DICE --htmlfile dice_nml.html --compversion 2.0
 
 # WW3
 echo "WW3"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/components/ww3/cime_config/namelist_definition_ww3.xml --comp WW3 --htmlfile ww3_nml.html
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/components/ww3/cime_config/namelist_definition_ww3.xml --comp WW3 --htmlfile ww3_nml.html
 
 # DWAV
 echo "DWAV"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/cime/src/components/data_comps/dwav/cime_config/namelist_definition_dwav.xml --comp DWAV --htmlfile dwav_nml.html --compversion 2.0
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/cime/src/components/data_comps/dwav/cime_config/namelist_definition_dwav.xml --comp DWAV --htmlfile dwav_nml.html --compversion 2.0
 
 # CISM
 echo "CISM"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/components/cism/bld/namelist_files/namelist_definition_cism.xml --comp CISM --htmlfile cism_nml.html
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/components/cism/bld/namelist_files/namelist_definition_cism.xml --comp CISM --htmlfile cism_nml.html
 
 # Driver
 echo "Driver"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/cime/src/drivers/mct/cime_config/namelist_definition_drv.xml --comp Driver --htmlfile drv_nml.html
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/cime/src/drivers/mct/cime_config/namelist_definition_drv.xml --comp Driver --htmlfile drv_nml.html
 
 # Driver fields
 echo "Driver fields"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/cime/src/drivers/mct/cime_config/namelist_definition_drv_flds.xml --comp Driver --htmlfile drv_fields_nml.html
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/cime/src/drivers/mct/cime_config/namelist_definition_drv_flds.xml --comp Driver --htmlfile drv_fields_nml.html
 
 # DESP 
 echo "DESP"
-python namelist-html.py --cesmmodel 2.1.5 --nmlfile $CESMROOT/cime/src/components/data_comps/desp/cime_config/namelist_definition_desp.xml --comp DESP --htmlfile desp_nml.html --compversion 2.0
+python namelist-html.py --cesmmodel 2.1.4 --nmlfile $CESMROOT/cime/src/components/data_comps/desp/cime_config/namelist_definition_desp.xml --comp DESP --htmlfile desp_nml.html --compversion 2.0
 
 
 
